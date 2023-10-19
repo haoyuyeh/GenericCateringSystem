1101016c4bc8fb47ebc557d6efb9e1770fa709d5
haoyuyeh <haoyuyeh@gmail.com>
2023-10-19 21:43:29 +0800

    finish the outlay of ConfigVC

---
+++ GenericCateringSystem/View/Log In Storyboard/ConfigVC.swift
@@ -0,0 +1,18 @@
+//
+//  ConfigVC.swift
+//  GenericCateringSystem
+//
+//  Created by Hao Yu Yeh on 2023/10/19.
+//
+
+import UIKit
+
+class ConfigVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}

9f7b2469284e70c6ce66948e313e87e9b37ff3fe
haoyuyeh <haoyuyeh@gmail.com>
2023-10-19 23:15:37 +0800

    construct a tab bar controller with 4 tabs for casher device

---
+++ GenericCateringSystem/View/Casher Device/CasherTabC.swift
@@ -0,0 +1,18 @@
+//
+//  CasherTabC.swift
+//  GenericCateringSystem
+//
+//  Created by Hao Yu Yeh on 2023/10/19.
+//
+
+import UIKit
+
+class CasherTabC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
